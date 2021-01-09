c545b86b4d2ba38f7bf0f05eef01d0f5b83c67fa
Suraj Vaddi <surajnvaddi@gmail.com>
2021-01-09 16:51:51 -0500

    Convert pressure control hpp and cpp

--- include/flight/modules/control_tasks/PressureControl.hpp
+++ include/flight/modules/control_tasks/PressureControl.hpp
@@ -1,29 +1,28 @@
-//
-// Created by adiv413 on 6/25/2020.
-//
-
 #ifndef FLIGHT_PRESSURECONTROL_HPP
 #define FLIGHT_PRESSURECONTROL_HPP
 
-#include <flight/modules/control_tasks/Control.hpp>
 #include <flight/modules/mcl/Registry.hpp>
 #include <flight/modules/mcl/Flag.hpp>
 #include <flight/modules/lib/Log.hpp>
 #include <flight/modules/lib/Enums.hpp>
 #include <unordered_map>
+#include "StageControl.hpp"
 
 class PressureControl : public Control {
 private:
-    Registry *registry;
-    Flag *flag;
-    const vector<pair<string, string>> matchups {
-        {"PT-2", "pressure_relief"}
-    };
+
+    vector<basic_string<char>> activate_stages;
+    map<basic_string<char>, ConfigValveInfo> valves;
+    map<basic_string<char>, ConfigSensorInfo> sensors;
+    vector<pair<SensorLocation, ValveLocation>> matchups;
+
     void check_pressure();
 
 public:
-    PressureControl() = default;
+    PressureControl();
     void begin();
     void execute();
+
+    void begin(json &config);
 };
 #endif //FLIGHT_PRESSURECONTROL_HPP
--- src/modules/control_tasks/PressureControl.cpp
+++ src/modules/control_tasks/PressureControl.cpp
@@ -1,17 +1,38 @@
-//
-// Created by adiv413 on 6/25/2020.
-//
-
 #include <Logger/logger_util.h>
 #include <flight/modules/control_tasks/PressureControl.hpp>
+#include <vector>
+#include <flight/modules/mcl/Config.hpp>
 
-void PressureControl::begin() {
+PressureControl::PressureControl(){
+    cout << "Pressure Control Initialized";
+}
+
+void PressureControl::begin(json& config) {
     log("Pressure control: Beginning");
 
     global_flag.log_info("response", {
-        {"header", "info"},
-        {"Description", "Pressure Control started"}
+            {"header",      "info"},
+            {"Description", "Pressure Control started"}
     });
+
+    this->activate_stages = global_config.pressure_control.active_stages;
+    this->valves = global_config.valves.list["solenoid"];
+    this->sensors = global_config.sensors.list["pressure"];
+    this->matchups.push_back(make_pair(SensorLocation::PT2, ValveLocation::PRESSURE_RELIEF));
+
+    for (pair<SensorLocation, ValveLocation> matched : this->matchups) {
+        try {
+            int(global_registry.sensors["pressure"][string(reinterpret_cast<const char *>(int(matched.first)))].normalized_value);
+        } catch (...) {
+            cout << "sensor at" << int(matched.first) << "not registered";
+        }
+
+        try {
+            int(global_registry.sensors["pressure"][string(reinterpret_cast<const char *>(int(matched.first)))].normalized_value); //not sure if this should be [0]
+        } catch (...) {
+            cout << "pressure_relief_valve not registered";
+        }
+    }
 }
 
 void PressureControl::execute() {
@@ -19,34 +40,23 @@ void PressureControl::execute() {
 }
 
 void PressureControl::check_pressure() {
-    for(const pair<string, string>& matched : this->matchups) {
-        auto registry_sensor = global_registry.sensors["pressure"][matched.first];
-        auto registry_valve = global_registry.valves["solenoid"][matched.second];
+    for(pair<SensorLocation, ValveLocation> matched : this->matchups) {
 
-        // Warning state
-        if (registry_sensor.status == SensorStatus::WARNING) {
-            // Open the valve
-            if (registry_valve.state == SolenoidState::CLOSED) {
-                global_flag.log_critical("response", {
-                    {"header", "info"},
-                    {"Description", "Pressure at " + matched.first + " is too high; opening " + matched.second + "."}
-                });
-
-                FlagValveInfo &valve_flag = global_flag.valves["solenoid"][matched.second];
-                valve_flag.actuation_type = ActuationType::OPEN_VENT;
-                valve_flag.actuation_priority = ValvePriority::MAX_TELEMETRY_PRIORITY;
+        if (global_registry.sensors["pressure"][string(reinterpret_cast<const char *>(int(matched.first)))].normalized_value >
+                global_config.sensors.list["pressure"][string(reinterpret_cast<const char *>(int(matched.first)))].boundaries.pressurization.safe.upper) {
+            if (global_registry.valves["solenoid"][string(
+                    reinterpret_cast<const char *>(int(matched.second)))].state == SolenoidState::CLOSED) {
+                global_registry.valves["solenoid"][string(reinterpret_cast<const char *>(int(
+                        matched.second)))].actuation_type = ActuationType::OPEN_VENT;
+                global_registry.valves["solenoid"][string(reinterpret_cast<const char *>(int(
+                        matched.second)))].actuation_priority = ValvePriority::PI_PRIORITY;
             }
-        } else if (registry_sensor.status == SensorStatus::SAFE) {
-            if (registry_valve.state == SolenoidState::OPEN) {
-                global_flag.log_critical("response", {
-                        {"header", "info"},
-                        {"Description", "Pressure at " + matched.first + " is safe; closing " + matched.second + "."}
-                });
-
-                FlagValveInfo &valve_flag = global_flag.valves["solenoid"][matched.second];
-                valve_flag.actuation_type = ActuationType::CLOSE_VENT;
-                valve_flag.actuation_priority = ValvePriority::MAX_TELEMETRY_PRIORITY;
-            } 
+        } else if (global_registry.sensors["pressure"][string(
+                reinterpret_cast<const char *>(int(matched.first)))].status == SensorStatus::SAFE) {
+            if (global_registry.valves["solenoid"][string(reinterpret_cast<const char *>(int(matched.second)))].state == SolenoidState::OPEN) {
+               global_registry.valves["solenoid"][string(reinterpret_cast<const char *>(int(matched.second)))].actuation_type = ActuationType::OPEN_VENT;
+               global_registry.valves["solenoid"][string(reinterpret_cast<const char *>(int(matched.second)))].actuation_priority = ValvePriority::PI_PRIORITY;
+            }
         }
     }
 }
