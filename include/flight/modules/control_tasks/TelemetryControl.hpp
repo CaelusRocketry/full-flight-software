c0dad6c8edda5dbf56485b0bca8b04b8436f372c
Srikar <srikarg89@gmail.com>
2021-03-28 21:04:36 -0400

    Uncomment files

--- include/flight/modules/control_tasks/TelemetryControl.hpp
+++ include/flight/modules/control_tasks/TelemetryControl.hpp
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-// //
-// // Created by Srikar on 4/15/2020.
-// //
-
-// #ifndef FLIGHT_TELEMETRYCONTROL_HPP
-// #define FLIGHT_TELEMETRYCONTROL_HPP
-
-// #include <flight/modules/control_tasks/Control.hpp>
-// #include <flight/modules/mcl/Registry.hpp>
-// #include <flight/modules/mcl/Flag.hpp>
-// #include <flight/modules/lib/Log.hpp>
-// #include <flight/modules/drivers/Telemetry.hpp>
-
-// class TelemetryControl : public Control {
-
-// typedef void (TelemetryControl::*functionType)(const vector<string>&);
-
-// private:
-//     unordered_map<string, functionType> functions;
-//     const unordered_map<string, vector<string>> arguments {
-//         {"heartbeat", {}},
-//         {"soft_abort", {}},
-//         {"undo_soft_abort", {}},
-//         {"solenoid_actuate", {"valve_location", "actuation_type", "priority"}},
-//         {"sensor_request", {"sensor_type", "sensor_location"}},
-//         {"valve_request", {"valve_type", "valve_location"}},
-//         {"progress", {}},
-//         {"test", {"response"}}
-//     };
-
-//     // Telemetry telemetry;
-
-//     void ingest(const Log& log);
-//     void heartbeat(const vector<string>& args);
-//     void soft_abort(const vector<string>& args);
-//     void undo_soft_abort(const vector<string>& args);
-//     void solenoid_actuate(const vector<string>& args);
-//     void sensor_request(const vector<string>& args);
-//     void valve_request(const vector<string>& args);
-//     void progress(const vector<string>& args);
-//     void test(const vector<string>& args);
-//     void make_functions();
-
-// public:
-//     TelemetryControl();
-//     void begin() override;
-//     void execute() override;
-// };
-// #endif //FLIGHT_TELEMETRYCONTROL_HPP
-=======
 #ifndef FLIGHT_TELEMETRYCONTROL_HPP
 #define FLIGHT_TELEMETRYCONTROL_HPP
 
@@ -94,4 +43,3 @@ public:
     void execute() override;
 };
 #endif //FLIGHT_TELEMETRYCONTROL_HPP
->>>>>>> 706881009a422b6710c09e093d976774888862b5
